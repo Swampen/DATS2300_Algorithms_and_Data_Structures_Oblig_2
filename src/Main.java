672af9ae5d526e7c67ea9249a2967767c11fcb7b
Ole-Martin <ole-ma@outlook.com>
2018-09-27 22:27:30 +0200

    Oppgave 0 complete

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Main {
+    public static void main(String[] args) {
+        String[] navn = {"Lars","Anders","Bodil","Kari","Per","Berit"};
+
+        Liste<String> liste1 = new DobbeltLenketListe<>(navn);
+        //Liste<String> liste2 = new TabellListe<>(navn);
+        //Liste<String> liste3 = new EnkeltLenketListe<>(navn);
+
+        DobbeltLenketListe.sorter(liste1, Comparator.naturalOrder());
+        //DobbeltLenketListe.sorter(liste2, Comparator.naturalOrder());
+        //DobbeltLenketListe.sorter(liste3, Comparator.naturalOrder());
+
+        System.out.println(liste1);  // [Anders, Berit, Bodil, Kari, Lars, Per]
+        //System.out.println(liste2);  // [Anders, Berit, Bodil, Kari, Lars, Per]
+        //System.out.println(liste3);  // [Anders, Berit, Bodil, Kari, Lars, Per]
+
+        // Tabellen navn er upåvirket:
+        System.out.println(Arrays.toString(navn));
+        // [Lars, Anders, Bodil, Kari, Per, Berit]
+    }
+
 }
